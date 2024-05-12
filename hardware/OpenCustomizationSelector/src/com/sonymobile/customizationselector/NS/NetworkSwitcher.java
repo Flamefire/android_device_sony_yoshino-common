6d5249b7bbedef68da42f0c89fe6d748b3087af9
Alexander Grund <Flamefire@users.noreply.github.com>
2024-05-12 19:59:45 +0200

    OpenCS: Refactor NetworkSwitcher
    - Move the state (subscription ID) to new class instead of passing it around
    - Cache the TelephonyManager & ContentResolver in the state class
    - Move util functions related to the state into that class
    - Use a single Handler instance (no recreations and less possibly concurrency issues)
    - Move listeners related to the state into that class and expose them as
      simple(r) functions
    
    Refactor Observers and usage
    - Accept `Handler` in ctor
    - Remove register/unregister functions from Slot/SubID-Observer
        Take all arguments in Ctor as the callback must be called only once
    - Use a boolean for the unlocked state
    
    Switcher logic in `NetworkSwitcher` is now clearer especially with
    better function names. Cleanup is ensured by clearing out the state class.
    
    Change-Id: Icf8b8a73b800cf8d3916a8ebc2f820be05c57a4a

--- hardware/OpenCustomizationSelector/src/com/sonymobile/customizationselector/NS/NetworkSwitcher.java
+++ hardware/OpenCustomizationSelector/src/com/sonymobile/customizationselector/NS/NetworkSwitcher.java
@@ -7,10 +7,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Handler;
 import android.os.IBinder;
-import android.provider.Settings;
-import android.telephony.RadioAccessFamily;
 import android.telephony.SubscriptionManager;
-import android.telephony.TelephonyManager;
 import com.sonymobile.customizationselector.CSLog;
 import com.sonymobile.customizationselector.CommonUtil;
 
@@ -30,29 +27,27 @@ public class NetworkSwitcher extends Service {
     }
 
     private static final String TAG = "NetworkSwitcher";
-    private static final String NS_LOWER_NETWORK = "ns_lowNet";
-    private static final String NS_PREFERRED = "ns_preferred";
 
-    private AirplaneModeObserver mAirplaneModeObserver;
-    private SimServiceObserver mSimServiceObserver;
-    // Set until the phone is unlocked
-    private BroadcastReceiver mUnlockObserver;
+    private boolean isUserUnlocked;
+    private SwitchState mState;
 
     @Override
     public void onCreate() {
         d("onCreate");
         final Context appContext = getApplicationContext();
 
-        mAirplaneModeObserver = new AirplaneModeObserver(appContext, new Handler(getMainLooper()));
-        mSimServiceObserver = new SimServiceObserver(appContext);
-        mUnlockObserver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                unregisterReceiver(mUnlockObserver);
-                mUnlockObserver = null;
-            }
-        };
-        registerReceiver(mUnlockObserver, new IntentFilter(Intent.ACTION_USER_UNLOCKED));
+        if (CommonUtil.isDirectBootEnabled()) {
+            isUserUnlocked = false;
+            registerReceiver(new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context context, Intent intent) {
+                        isUserUnlocked = true;
+                        unregisterReceiver(this);
+                    }
+                }, new IntentFilter(Intent.ACTION_USER_UNLOCKED)
+            );
+        } else
+            isUserUnlocked = true;
 
         // Start process
         try {
@@ -63,7 +58,7 @@ public class NetworkSwitcher extends Service {
 
             int subID = CommonUtil.getSubID(appContext);
             if (subID == SubscriptionManager.INVALID_SUBSCRIPTION_ID)
-                new SubIdObserver(appContext).register(this::initProcess);
+                new SubIdObserver(appContext, this::initProcess);
             else
                 initProcess(subID);
         } catch (Exception e) {
@@ -78,142 +73,82 @@ public class NetworkSwitcher extends Service {
         return START_STICKY;
     }
 
+    /** Stop the Service after it has started processing by {@link initProcess} */
+    private void stopProcess() {
+        mState.stop();
+        mState = null;
+        stopSelf();
+    }
+
+    /** Start the switching process for the given subscription ID */
     private void initProcess(int subID) {
-        if (CommonUtil.isSIMLoaded(getApplicationContext(), subID))
-            new Handler(getMainLooper()).postDelayed(() -> switchDown(subID), 1400);
-        else {
-            new SlotObserver(getApplicationContext()).register(subID,
-                    () -> new Handler(getMainLooper()).postDelayed(() -> switchDown(subID), 1400));
+        if (subID == SubscriptionManager.INVALID_SUBSCRIPTION_ID) {
+            e("Error: Invalid subID");
+            stopSelf();
+        } else {
+            mState = new SwitchState(subID, getApplicationContext(), new Handler(getMainLooper()));
+            mState.waitForSim(() -> onSIMLoaded());
         }
     }
 
-    private void switchDown(int subID) {
-        if (subID < 0) {
-            d("switchDown: Error, invalid subID");
-            stopSelf();
-            return;
-        }
-
-        TelephonyManager tm = getSystemService(TelephonyManager.class).createForSubscriptionId(subID);
-
-        long currentNetwork = getPreferredNetwork(tm);
-        if (isLTE(currentNetwork)) {
-            setOriginalNetwork(subID, currentNetwork);
-            changeNetwork(tm, subID, getLowerNetwork());
-
-            if (CommonUtil.isDirectBootEnabled() && mUnlockObserver != null) {
-                // Delay resetting the network until phone is unlocked.
-                // The current unlock observer is no longer required
-                unregisterReceiver(mUnlockObserver);
-                mUnlockObserver = null;
-                registerReceiver(new BroadcastReceiver() {
-                    @Override
-                    public void onReceive(Context context, Intent intent) {
-                        unregisterReceiver(this);
-                        handleConnection(tm, subID);
-                    }
-                }, new IntentFilter(Intent.ACTION_USER_UNLOCKED));
-            } else
-                handleConnection(tm, subID);
-        } else {
-            d("Network is not LTE, no work.");
-            stopSelf();
-        }
+    /** To be called once the SIM is ready */
+    private void onSIMLoaded() {
+        d("SIM loaded for subID " + mState.mSubID);
+        mState.postDelayed(() -> switchDown(), 1400);
     }
 
-    private void handleConnection(TelephonyManager tm, int subID) {
-        if (isAirplaneModeOn()) {
-            mAirplaneModeObserver.register(() -> {
-                if (isAirplaneModeOn())
-                    mSimServiceObserver.unregister();
-                else {
-                    mSimServiceObserver.register(subID, () -> {
-                        mAirplaneModeObserver.unregister();
-                        changeNetwork(tm, subID, getOriginalNetwork(subID));
-                        stopSelf();
-                    });
-                }
-            });
-        } else {
-            if (CommonUtil.hasSignal(tm)) {
-                changeNetwork(tm, subID, getOriginalNetwork(subID));
-                stopSelf();
-            } else {
-                mSimServiceObserver.register(subID, () -> {
-                    changeNetwork(tm, subID, getOriginalNetwork(subID));
-                    stopSelf();
-                });
+    /** Switch to the lower network and continue once the user unlocked the phone */
+    private void switchDown() {
+        if (SwitchState.isLTE(mState.getPreferredNetwork())) {
+            if (!mState.changeNetworkDown()) {
+                e("Error: Failed changing to lower network");
+                stopProcess();
+                return;
             }
+
+            // Delay resetting the network until phone is unlocked.
+            if (isUserUnlocked)
+                startWaitForLowerNetwork();
+            else {
+                d("Waiting for user unlock");
+                registerReceiver(new BroadcastReceiver() {
+                        @Override
+                        public void onReceive(Context context, Intent intent) {
+                            unregisterReceiver(this);
+                            startWaitForLowerNetwork();
+                        }
+                    }, new IntentFilter(Intent.ACTION_USER_UNLOCKED)
+                );
+            }
+        } else {
+            i("Network is not LTE, no work.");
+            stopProcess();
         }
     }
 
-    /**
-     * The method to change the network
-     *
-     * @param tm             {@link TelephonyManager} specific to subID
-     * @param subID          the subscription ID from [subscriptionsChangedListener]
-     * @param newNetwork     network to change to
-     */
-    private void changeNetwork(TelephonyManager tm, int subID, long newNetwork) {
-        d("changeNetwork: To be changed to = " + TelephonyManager.convertNetworkTypeBitmaskToString(newNetwork));
-
-        try {
-            tm.setAllowedNetworkTypesForReason(TelephonyManager.ALLOWED_NETWORK_TYPES_REASON_USER, newNetwork);
-        } catch (IllegalStateException e) {
-            d("changeNetwork: Failed to change network, no telephony service!");
-            return;
-	}
-        d("changeNetwork: Successfully changed to " + TelephonyManager.convertNetworkTypeBitmaskToString(newNetwork));
+    /** Start waiting for a signal on the lower network */
+    private void startWaitForLowerNetwork() {
+        d("Waiting for lower network");
+        mState.waitForSignal(() -> onLowerSignal());
     }
 
-    /**
-     * Get the current in-use network mode preference, i.e. RAF
-     * <p>
-     * There are no defaults other than {@link #INVALID_NETWORK}
-     */
-    private @TelephonyManager.NetworkTypeBitMask long getPreferredNetwork(TelephonyManager tm) {
-        return tm.getAllowedNetworkTypesForReason(TelephonyManager.ALLOWED_NETWORK_TYPES_REASON_USER);
-    }
-
-    /**
-     * Get the original network mode preference
-     *
-     * @return Stored value, defaults to LTE, GSM & WCDMA
-     */
-    private @TelephonyManager.NetworkTypeBitMask long getOriginalNetwork(int subID) {
-        return Settings.System.getLong(getApplicationContext().getContentResolver(), NS_PREFERRED + subID,
-                                       RadioAccessFamily.getRafFromNetworkType(TelephonyManager.NETWORK_MODE_LTE_GSM_WCDMA));
-    }
-
-    private void setOriginalNetwork(int subID, @TelephonyManager.NetworkTypeBitMask long network) {
-        Settings.System.putLong(getApplicationContext().getContentResolver(), NS_PREFERRED + subID, network);
-    }
-
-    /**
-     * Returns whether @param network is LTE or not
-     */
-    private boolean isLTE(@TelephonyManager.NetworkTypeBitMask long network) {
-        return (network & TelephonyManager.NETWORK_CLASS_BITMASK_4G) != 0;
-    }
-
-    /**
-     * This method returns the lower network to switch to
-     */
-    private int getLowerNetwork() {
-        return RadioAccessFamily.getRafFromNetworkType(
-            Settings.System.getInt(getApplicationContext().getContentResolver(), NS_LOWER_NETWORK, TelephonyManager.NETWORK_MODE_WCDMA_PREF));
-    }
-
-    /**
-     * Gets the state of Airplane Mode.
-     *
-     * @return true if enabled.
-     */
-    private boolean isAirplaneModeOn() {
-        return Settings.System.getInt(getApplicationContext().getContentResolver(), Settings.Global.AIRPLANE_MODE_ON, 0) != 0;
+    private void onLowerSignal() {
+        if (mState.changeToOriginalNetwork())
+            d("Changed back to original network");
+        else
+            e("Failed changing back to original network");
+        stopProcess();
     }
 
     private void d(String msg) {
         CSLog.d(TAG, msg);
+    }
+
+    private void i(String msg) {
+        CSLog.i(TAG, msg);
+    }
+
+    private void e(String msg) {
+        CSLog.e(TAG, msg);
     }
 }
---
+++ hardware/OpenCustomizationSelector/src/com/sonymobile/customizationselector/NS/SwitchState.java
@@ -0,0 +1,182 @@
+package com.sonymobile.customizationselector.NS;
+
+import com.sonymobile.customizationselector.CSLog;
+import com.sonymobile.customizationselector.CommonUtil;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.Handler;
+import android.provider.Settings;
+import android.telephony.RadioAccessFamily;
+import android.telephony.TelephonyManager;
+import android.telephony.SubscriptionManager;
+import com.android.internal.telephony.RILConstants;
+
+/**
+ * State of the Network switch operation helper methods
+ * Based on work of shank03
+ *
+ * @author Flamefire
+ */
+public class SwitchState {
+    private static final String TAG = "NetworkSwitcher";
+
+    private static final String NS_LOWER_NETWORK = "ns_lowNet";
+    private static final String NS_PREFERRED = "ns_preferred";
+
+    public final int mSubID;
+    private final Context mContext;
+    private final ContentResolver mContent;
+    private final Handler mHandler;
+    private final TelephonyManager mTm;
+    private final AirplaneModeObserver mAirplaneModeObserver;
+    private final SimServiceObserver mSimServiceObserver;
+    private SlotObserver mSlotObserver;
+
+    public SwitchState(int subID, Context appContext, Handler handler) throws IllegalArgumentException {
+        if (subID == SubscriptionManager.INVALID_SUBSCRIPTION_ID)
+            throw new IllegalArgumentException();
+        mSubID = subID;
+        mContext = appContext;
+        mContent = mContext.getContentResolver();
+        mHandler = handler;
+        mTm = mContext.getSystemService(TelephonyManager.class).createForSubscriptionId(mSubID);
+        mAirplaneModeObserver = new AirplaneModeObserver(mHandler, mContent);
+        mSimServiceObserver = new SimServiceObserver(mHandler, mTm);
+    }
+
+    /** Stop all observers and any pending messages */
+    public void stop() {
+        mAirplaneModeObserver.unregister();
+        mSimServiceObserver.unregister();
+        if (mSlotObserver != null) {
+            mSlotObserver.unregister();
+            mSlotObserver = null;
+        }
+        mHandler.removeCallbacksAndMessages(null);
+    }
+
+
+    /** Run the callback once the SIM is loaded */
+    public void waitForSim(SlotObserver.Listener callback) {
+        if (CommonUtil.isSIMLoaded(mContext, mSubID))
+            callback.onConnected();
+        else
+            mSlotObserver = new SlotObserver(mContext, mHandler, mSubID, callback);
+    }
+
+    /** Run the callback once we have a signal */
+    public void waitForSignal(Runnable callback) {
+        if (!isAirplaneModeOn() && hasSignal())
+            callback.run();
+        else {
+            SimServiceObserver.Listener signalListener = () -> {
+                mAirplaneModeObserver.unregister();
+                mSimServiceObserver.unregister();
+                callback.run();
+            };
+            if (!isAirplaneModeOn())
+                mSimServiceObserver.register(signalListener);
+            mAirplaneModeObserver.register(() -> {
+                if (isAirplaneModeOn())
+                    mSimServiceObserver.unregister();
+                else
+                    mSimServiceObserver.register(signalListener);
+            });
+        }
+    }
+
+    public Handler getHandler() {
+        return mHandler;
+    }
+
+    public void postDelayed(Runnable r, long delay) {
+        mHandler.postDelayed(r, delay);
+    }
+
+    /** Change to the lower network storing the original one */
+    public boolean changeNetworkDown() {
+        return setOriginalNetwork(getPreferredNetwork()) && changeNetwork(getLowerNetwork());
+    }
+
+    /** Change back to the original network */
+    public boolean changeToOriginalNetwork() {
+        return changeNetwork(getOriginalNetwork());
+    }
+
+    /**
+     * Change the network
+     *
+     * @param newNetwork     network to change to
+     */
+    private boolean changeNetwork(@TelephonyManager.NetworkTypeBitMask long newNetwork) {
+        final String networkString = TelephonyManager.convertNetworkTypeBitmaskToString(newNetwork);
+
+        try {
+            mTm.setAllowedNetworkTypesForReason(TelephonyManager.ALLOWED_NETWORK_TYPES_REASON_USER, newNetwork);
+            d("changeNetwork: Successfully changed to " + networkString);
+            return true;
+        } catch (Exception e) {
+            d("changeNetwork: Failed changing to " + networkString + ": " + e.getMessage());
+            return false;
+        }
+	}
+
+    /**
+     * Get the current in-use network mode preference, i.e. RAF
+     * <p>
+     * There are no defaults other than {@link #INVALID_NETWORK}
+     */
+    private @TelephonyManager.NetworkTypeBitMask long getPreferredNetwork() {
+        return mTm.getAllowedNetworkTypesForReason(TelephonyManager.ALLOWED_NETWORK_TYPES_REASON_USER);
+    }
+
+    /**
+     * Get the original network mode preference
+     *
+     * @return Stored value, defaults to LTE, GSM & WCDMA
+     */
+    private @TelephonyManager.NetworkTypeBitMask long getOriginalNetwork() {
+        return Settings.System.getLong(mContent, NS_PREFERRED + mSubID,
+                                       RadioAccessFamily.getRafFromNetworkType(TelephonyManager.NETWORK_MODE_LTE_GSM_WCDMA));
+    }
+
+    private boolean setOriginalNetwork(@TelephonyManager.NetworkTypeBitMask long network) {
+        return Settings.System.putLong(mContent, NS_PREFERRED + mSubID, network);
+    }
+
+    /**
+     * This method returns the lower network to switch to
+     */
+    public @TelephonyManager.NetworkTypeBitMask long getLowerNetwork() {
+        return RadioAccessFamily.getRafFromNetworkType(
+            Settings.System.getInt(mContent, NS_LOWER_NETWORK, TelephonyManager.NETWORK_MODE_WCDMA_PREF));
+    }
+
+    /**
+     * Gets the state of Airplane Mode.
+     *
+     * @return true if enabled.
+     */
+    private boolean isAirplaneModeOn() {
+        return Settings.System.getInt(mContent, Settings.Global.AIRPLANE_MODE_ON, 0) != 0;
+    }
+
+    /**
+     * Return true if there is a (network) signal for the SIM
+     */
+    private boolean hasSignal() {
+        return CommonUtil.hasSignal(mTm);
+    }
+
+    /**
+     * Returns whether @param network is LTE or not
+     */
+    private boolean isLTE(@TelephonyManager.NetworkTypeBitMask long network) {
+        return (network & TelephonyManager.NETWORK_CLASS_BITMASK_4G) != 0;
+    }
+
+    private void d(String msg) {
+        CSLog.d(TAG, msg);
+    }
+}
