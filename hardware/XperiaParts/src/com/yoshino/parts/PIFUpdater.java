687f4e543f254e8b8618271fd9974f1acfe2fb56
Alexander Grund <Flamefire@users.noreply.github.com>
2024-11-23 11:33:59 +0100

    Revert "Write custom PIF to Downloads folder"
    This reverts commit ce2fa8738fe0f690fc3ad9ab31083702066e4ba9.
    
    Change-Id: I472f6c65db45d158073818e57ca5a8f0f288ae0b

--- hardware/XperiaParts/src/com/yoshino/parts/PIFUpdater.java
+++ hardware/XperiaParts/src/com/yoshino/parts/PIFUpdater.java
@@ -14,13 +14,8 @@
 
  package com.yoshino.parts;
 
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Environment;
-import android.os.SystemProperties;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -28,7 +23,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -45,147 +39,48 @@ public class PIFUpdater extends AsyncTask<Void, Void, String> {
 
     private static final String PIF_DOWNLOAD_URL =
             "https://raw.githubusercontent.com/Flamefire/lineageos_lilac/refs/heads/main/tools/pif.json";
-    private static final String CUSTOM_PIF_NAME = "pif.json";
-    private static final Uri STORAGE_URI = MediaStore.Downloads.EXTERNAL_CONTENT_URI;
+    private static final String CUSTOM_PIF_PATH = "/sdcard/pif.json";
 
     private final Context mContext;
     private final Listener mListener;
-
-    private static String readInputStream(InputStream stream) throws IOException{
-        try(InputStreamReader reader = new InputStreamReader(stream)) {
-            char[] buffer = new char[128];
-            StringBuilder content = new StringBuilder();
-            for (int numRead; (numRead = reader.read(buffer, 0, buffer.length)) > 0; )
-                content.append(buffer, 0, numRead);
-            return content.toString();
-        }
-    }
-
-    private Uri getCustomPIFConfigURI() {
-        final String[] projection = {
-            MediaStore.Downloads._ID,
-            MediaStore.Downloads.DISPLAY_NAME
-        };
-
-        final String selection = MediaStore.Downloads.DISPLAY_NAME + " = ?";
-        final String[] selectionArgs = new String[]{CUSTOM_PIF_NAME};
-
-        final Cursor cursor = mContext.getContentResolver().query(STORAGE_URI, projection, selection, selectionArgs, null);
-        if (cursor != null && cursor.moveToFirst()) {
-            final long id = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Downloads._ID));
-            return Uri.withAppendedPath(STORAGE_URI, String.valueOf(id));
-        } else
-            return null;
-    }
-
-    private String readCustomPIFConfigFile() {
-        final File[] locations = new File[] {
-            new File("/sdcard/pif.json"),
-            new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "pif.json"),
-            getCustomPIFLocation()
-        };
-        for (File file: locations) {
-            Log.w(TAG, "Checking file " + file);
-            if (!file.exists())
-                continue;
-            Log.w(TAG, "Exists!");
-            try (InputStream is = new FileInputStream(file)){
-                String content = readInputStream(is);
-                Log.w(TAG, "File read: " + content);
-            } catch (IOException e) {
-                Log.e(TAG, "Failed reading " + e);
-            }
-        }
-
-        final Uri provUri = Uri.parse("content://com.yoshino.parts.pifprovider/pif");
-        try (InputStream inputStream = mContext.getContentResolver().openInputStream(provUri)) {
-            if (inputStream != null) {
-                final String content = readInputStream(inputStream);
-                Log.w(TAG, "Read from " + provUri + ": " + content);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        final Uri fileUri = getCustomPIFConfigURI();
-        if (fileUri == null)
-            return null;
-
-        try (InputStream inputStream = mContext.getContentResolver().openInputStream(fileUri)) {
-            return readInputStream(inputStream);
-        } catch (Exception e) {
-            Log.e(TAG, "Failed to read from Uri: " + e);
-            return null;
-        }
-    }
-
-    public boolean saveCustomPIFConfigFile(String name, String jsonContent) {
-        final Uri existingFile = getCustomPIFConfigURI();
-        if (existingFile != null)
-            mContext.getContentResolver().delete(existingFile, null, null);
-
-        final ContentValues contentValues = new ContentValues();
-        contentValues.put(MediaStore.Downloads.DISPLAY_NAME, name);
-        contentValues.put(MediaStore.Downloads.MIME_TYPE, "application/json");
-        contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS);
-        contentValues.put(MediaStore.Downloads.IS_PENDING, 1);
-
-        final Uri uri = mContext.getContentResolver().insert(STORAGE_URI, contentValues);
-
-        if (uri == null)
-            return false;
-        
-        try(OutputStream outputStream = mContext.getContentResolver().openOutputStream(uri)) {
-            if (outputStream != null) {
-                outputStream.write(jsonContent.getBytes());
-                outputStream.flush();
-            }
-        } catch (IOException e) {
-            Log.e(TAG, "Failed to write content: " + e);
-        }
-
-        contentValues.clear();
-        contentValues.put(MediaStore.Downloads.IS_PENDING, 0);
-        mContext.getContentResolver().update(uri, contentValues, null, null);
-        return true;
-    }
 
     public PIFUpdater(Context context, Listener listener) {
         mContext = context;
         mListener = listener;
     }
 
-    public File getCustomPIFLocation() {
-        return new File(mContext.getCacheDir(), CUSTOM_PIF_NAME);
-    }
-
     @Override
     protected String doInBackground(Void... voids) {
+        File tempFile = null;
         try {
             final URL url = new URL(PIF_DOWNLOAD_URL);
+            tempFile = File.createTempFile("tempDownload", ".json", mContext.getCacheDir());
 
-            final String newConfig = downloadFile(url);
-            if (newConfig == null)
+            if (!downloadFile(url, tempFile))
                 return "Download failed";
             Log.i(TAG, "Downloaded PIF from " + url);
 
-            final String oldConfig = readCustomPIFConfigFile();
-
-            if (oldConfig != null) {
-                if (newConfig.equals(oldConfig))
+            final File customPifFile = new File(CUSTOM_PIF_PATH);
+            if (customPifFile.exists()) {
+                Log.i(TAG, "Checking existing config");
+                if (areFilesEqual(tempFile, customPifFile))
                     return "Config is already up-to-date.";
                 Log.i(TAG, "Update required");
+                if (!customPifFile.delete())
+                    return "Failed to remove old config at " + customPifFile.getPath();
             }
 
-            if (saveCustomPIFConfigFile(CUSTOM_PIF_NAME, newConfig))
+            if (tempFile.renameTo(customPifFile))
                 return "Config updated";
             else
-                return "Failed to write to config file";
+                return "Failed to write to config file at " + customPifFile.getPath();
 
         } catch (Exception e) {
             e.printStackTrace();
-            return "An error occurred?";
+            return "An error occurred";
+        } finally {
+            if (tempFile != null && tempFile.exists())
+                tempFile.delete();
         }
     }
 
@@ -202,23 +97,28 @@ public class PIFUpdater extends AsyncTask<Void, Void, String> {
         mListener.OnCompleted(customConfig);
     }
 
-    private static String downloadFile(URL url) {
+    private static boolean downloadFile(URL url, File destination) {
         try {
             final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.connect();
             final int responseCode = connection.getResponseCode();
             if (responseCode != HttpURLConnection.HTTP_OK) {
                 Log.e(TAG, "Connection failed: Response code " + responseCode);
-                return null;
+                return false;
             }
-            try (InputStream inputStream = connection.getInputStream()) {
-                return readInputStream(inputStream);
+            try (InputStream inputStream = connection.getInputStream();
+                OutputStream outputStream = new FileOutputStream(destination)) {
+                final byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1)
+                    outputStream.write(buffer, 0, bytesRead);
             }
         } catch (Exception e) {
             e.printStackTrace();
             Log.e(TAG, "Download failed: Error " + e);
-            return null;
+            return false;
         }
+        return true;
     }
 
     private static boolean areFilesEqual(File file1, File file2) throws Exception {
